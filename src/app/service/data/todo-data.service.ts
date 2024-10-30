98c2e53e8123465737f09a6b777d94e9664dc0f3
YSmal <yurii.smal@opentext.com>
2024-10-30 18:15:20 +0200

    Still init

---
+++ src/app/service/data/todo-data.service.ts
@@ -0,0 +1,25 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Todo} from "../../list-todos/list-todos.component";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoDataService {
+
+  constructor(private http: HttpClient) {
+  }
+
+
+  retrieveAllTodos(name: string) {
+    return this.http.get<Todo[]>(`http://localhost:8080/users/${name}/todos`);
+  }
+
+  deleteTodo(username: string, id: number) {
+    return this.http.delete<Todo>(`http://localhost:8080/users/${username}/todos/${id}`);
+  }
+
+  // updateTodo(username: string, id: number) {
+  //   return this.http.put<Todo>(`http://localhost:8080/users/${username}/todos/${id}`);
+  // }
+}
