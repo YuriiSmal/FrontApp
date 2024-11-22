f808346a9eac6c26576355d26b08da5b77a0d56e
YSmal <yurii.smal@opentext.com>
2024-11-22 16:07:18 +0200

    While most of it works I will do this commit =)

--- src/app/presentation/presentation.component.ts
+++ src/app/presentation/presentation.component.ts
@@ -1,16 +1,13 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, HostListener, OnDestroy, OnInit} from '@angular/core';
 import {MenuFooterService} from "../service/menu-footer/menu-footer.service";
-
 
 @Component({
     selector: 'app-presentation',
     templateUrl: './presentation.component.html',
-    styleUrl: './presentation.component.css',
-
+    styleUrls: ['./presentation.component.css'],  // виправлено styleUrl на styleUrls
 })
 export class PresentationComponent implements OnInit, OnDestroy {
-    constructor(private menuFooterService: MenuFooterService) {
-    }
+    constructor(private menuFooterService: MenuFooterService) {}
 
     ngOnInit(): void {
         this.menuFooterService.setMenuVisibility(false);
@@ -20,5 +17,43 @@ export class PresentationComponent implements OnInit, OnDestroy {
     ngOnDestroy(): void {
         this.menuFooterService.setMenuVisibility(true);
         this.menuFooterService.setFooterVisibility(true);
+    }
+
+    activeSection: string = '';
+
+    // Слухаємо подію прокручування для відслідковування поточного розділу
+    @HostListener('window:scroll', [])
+    onWindowScroll() {
+        const sections = ['about', 'course', 'price', 'contact'];
+        let currentSection = '';
+
+        for (const section of sections) {
+            const sectionElement = document.getElementById(section);
+            if (sectionElement) {
+                const rect = sectionElement.getBoundingClientRect();
+                // Перевіряємо, чи секція знаходиться в межах видимості
+                if (rect.top <= window.innerHeight / 2 && rect.bottom >= window.innerHeight / 2) {
+                    currentSection = section;
+                    break;
+                }
+            }
+        }
+
+        // Оновлюємо активну секцію
+        if (currentSection !== this.activeSection) {
+            this.activeSection = currentSection;
+        }
+    }
+
+    scrollToSection(sectionId: string, event: Event) {
+        event.preventDefault(); // Запобігаємо стандартній поведінці браузера
+        const sectionElement = document.getElementById(sectionId);
+
+        if (sectionElement) {
+            sectionElement.scrollIntoView({
+                behavior: 'smooth', // Плавна прокрутка
+                block: 'start', // Початок секції
+            });
+        }
     }
 }
